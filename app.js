efee13eb4e5ada26223d26d3c9808baec8817ffc
ROSE <brain10master@gmail.com>
2017-02-17 15:16:04 +0900

    Add config files

--- app.js
+++ app.js
@@ -14,17 +14,6 @@ angular.module('DictApp',["firebase"])
   }
 });
 
-// //** SETTINGS UP FIREBASE **/
-// var config = {
-//   apiKey: "AIzaSyCC_PMzSnYuou0u_nuuYTt_H27XEMhru4w",
-//   authDomain: "vfdict.firebaseapp.com",
-//   databaseURL: "https://vfdict.firebaseio.com/",
-//   storageBucket: "vfdict.appspot.com",
-//   messagingSenderId: "1051867458155"
-// };
-// firebase.initializeApp(config);
-
-
 // listComponentController.$inject=['listService', '$scope', '$firebaseArray'];
 function listComponentController($scope, $firebaseArray, listService){
   var $ctrl= this;
---
+++ config/config.js
@@ -0,0 +1,9 @@
+//** SETTINGS UP FIREBASE **/
+var config = {
+  apiKey: "AIzaSyCC_PMzSnYuou0u_nuuYTt_H27XEMhru4w",
+  authDomain: "vfdict.firebaseapp.com",
+  databaseURL: "https://vfdict.firebaseio.com/",
+  storageBucket: "vfdict.appspot.com",
+  messagingSenderId: "1051867458155"
+};
+firebase.initializeApp(config);
